b6bfaa47c3a0648e4b0514c3ef863da69e5f6a95
xatt <111096156+MaliciousXatt@users.noreply.github.com>
2023-01-10 16:46:07 +0100

    Add files via upload
    Made the program usable for Windows and Unix.
    Also added features like self made functions for webhook.

--- KittyHook.py
+++ KittyHook.py
@@ -1,10 +1,51 @@
-import os
-webhook = """https://api.telegram.org/bot<BOT-TOKEN>/sendMessage\?chat_id\=<CHAT-ID>\&text\=""" # This is your webhook that ends with text\=
+import os                   # IMPORTING
+from sys import platform    # NEEDED
+import requests             # MODULES
+
+# Specify root location for your wordlists | FOR WINDOWS USE double \\ !!
+wl = r'C:\\Users\\<EXAMPLE-USER>\\Documents\\wordlists\\'
+
+# Function, checking if you are using Windows or Unix-ish
+def machinetype():
+    if platform != "win32":
+        return 1
+    else:
+        return 2
+if machinetype() != 1:
+    ps = "powershell .\\"
+    exe = ".exe"
+else:
+    ps = ""
+    exe = ""
+
+# FILL IN THE <BETWEEN>
+def webhook(text):
+    url = "https://api.telegram.org/bot<BOT-TOKEN>/sendMessage?chat_id=<CHAT-ID>="+text
+    req = requests.post(url)
+    print(req.text)
+
+# This makes you a valid hacker
+print("""
+    __ __ _ __  __        __  __            __                                 
+   / //_/(_) /_/ /___  __/ / / /___  ____  / /__                               
+  / ,<  / / __/ __/ / / / /_/ / __ \/ __ \/ //_/                               
+ / /| |/ / /_/ /_/ /_/ / __  / /_/ / /_/ / ,<                                  
+/_/ |_/_/\__/\__/\__, /_/ /_/\____/\____/_/|_|                                 
+ _______________/____/_______________________________________                  
+/_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/_  __      __  __ 
+   / /_  __  ___    /  |/  /___ _/ (_)____(_)___  __  ______| |/ /___ _/ /_/ /_
+  / __ \/ / / (_)  / /|_/ / __ `/ / / ___/ / __ \/ / / / ___/   / __ `/ __/ __/
+ / /_/ / /_/ /    / /  / / /_/ / / / /__/ / /_/ / /_/ (__  )   / /_/ / /_/ /_  
+/_.___/\__, (_)  /_/  /_/\__,_/_/_/\___/_/\____/\__,_/____/_/|_\__,_/\__/\__/  
+      /____/                                                                  
+      """)
+
 start = input('Do you want to start? Y or N\n') # Check if you want to run
 if start.lower() != 'y':
     print("Bye")
     exit()
 else: # Gonna run
+    print("MAKE SURE THIS PROGRAM IS IN YOUR HASHCAT FILE LOCATION")
     print("""Choose out of:
     md5 = 0     
     SHA1 = 100
@@ -12,19 +53,19 @@ else: # Gonna run
           """)
     hashtype = int(input('Enter hash type:\n'))
     hashpath = input('Enter hash path:\n')
-    wordlist = input("Enter wordlist path:\n")
-    command = f"hashcat -m {hashtype} {hashpath} --wordlist {wordlist}"
+    wordlist = wl+input(f"Enter wordlist path:\n{wl}")
+    command = f"hashcat{exe} -m {hashtype} {hashpath} --wordlist {wordlist}"
     run = input(f'Do you want to run {command}?\nY or N\n')
     if run.lower() != 'y': # Check if you agree with the syntax
         choice = int(input("Make a choice:\n1: More wordlists\n2: Add extension\n3: Benchmark\n"))
-        if choice in {1,2,3}:
+        if choice in {1,2,3}: # You can finally make choices
             if choice == 1:
                 wordlist2 = input("Enter second wordlist path\n")
-                command1 = f"hashcat -m {hashtype} {hashpath} --wordlist {wordlist},{wordlist2}"
+                command1 = f"hashcat{exe} -m {hashtype} {hashpath} --wordlist {wordlist},{wordlist2}"
                 runnie = input(f"Want to run {command1}?\nY or N\n")
-                if runnie == "y":
-                    os.system(f'{command1}')
-                    os.system(f'curl --noproxy -xPOST {webhook}Done+cracking.')
+                if runnie.lower() == "y":
+                    os.system(f'{ps}{command1}')
+                    webhook("Done+Cracking") # Returning the webhook, with text
                 else:
                     ext = input("Add syntax\n")
                     command3 = f"{command1} {ext}"
@@ -32,24 +73,24 @@ else: # Gonna run
                     if run3 != "Y":
                         exit()
                     else:
-                        os.system(f'{command3}')
-                        os.system(f'curl --noproxy -xPOST {webhook}Done+cracking.')
+                        os.system(f'{ps}{command3}')
+                        webhook("Done+Cracking")
             elif choice == 2:
-                ext = input("Add syntax\n")
+                ext = input(f"Add syntax\n{command}")
                 command4 = f"{command} {ext}"
                 run4 = input(f"Run: {command4}?\nY or N\n")
-                if run4 != "Y":
+                if run4.lower() != "y":
                     exit()
                 else:
-                    os.system(f'{command4}')
-                    os.system(f'curl --noproxy -xPOST {webhook}Done+cracking.') 
+                    os.system(f'{ps}{command4}')
+                    webhook("Done+Cracking")
             elif choice == 3:
-                command5 = "hashcat -m 100 -b"
+                command5 = f"hashcat{exe} -m 100 -b"
                 print(f"{command5}")
                 choice2 = input("Want to add more syntax?\nY or N\n")
                 if choice2.lower() != "y":
-                    os.system(f'{command5}')
-                    os.system(f'curl --noproxy -xPOST {webhook}Benchmark+Done.')
+                    os.system(f'{ps}{command5}')
+                    webhook("Benchmark+Done")
                 else:
                     syntax2 = input(f"Enter syntax:\n{command5}")
                     print(f"{command5}{syntax}")
@@ -57,10 +98,14 @@ else: # Gonna run
                     if check.lower() != "y":
                         exit()
                     else:
-                        os.system(f'{command5}{syntax2}')
-                        os.system(f'curl --noproxy -xPOST {webhook}Benchmark+Done.')
+                        os.system(f'{ps}{command5}{syntax2}')
+                        webhook("Benchmark+Done")
         else:
-            print("Does not exist")
+            print("Does not exist") # You did something wrong!
     else:
-        os.system(f'{command}')
-        os.system(f'curl --noproxy -xPOST {webhook}Done+cracking.')
+        os.system(f'{ps}{command}')
+        webhook("Done+Cracking.")
+
+print("""
+♡ ♡ ♡ ♡ ♡ ♡ ♡
+      """)
